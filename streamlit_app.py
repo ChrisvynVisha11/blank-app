0a7dd1f1dbe5b3ddc3f9a07067dda0328a730994
ChrisvynVisha11 <chrisvynvisha11@gmail.com>
2026-04-01 16:24:18 +0000

    2nd edit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,7 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("🎈 Household Renovation Recommendation")
+
 import streamlit as st
 import pandas as pd
 
